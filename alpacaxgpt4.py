ebcc69a6647a5624ee757d90d96f864ddd61abe4
Enju <97325184+Naugustogi@users.noreply.github.com>
2023-04-05 18:33:53 +0200

    Update alpacaxgpt4.py

--- alpacaxgpt4.py
+++ alpacaxgpt4.py
@@ -7,7 +7,6 @@ from pyllamacpp.model import Model
 
 print("lade model")
 start_time = time.time()
-model = Model(ggml_model='pytorch_model.bin')
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 print("start discord package")
@@ -23,23 +22,25 @@ token = "" #smuggy
 intents = discord.Intents.default() #all
 client = discord.Client(intents=intents)
 print("apply context")
-
-
-print("torch.cuda.memory_allocated: %fGB" %
-      (torch.cuda.memory_allocated(0)/1024/1024/1024))
-print("torch.cuda.memory_reserved: %fGB" %
-      (torch.cuda.memory_reserved(0)/1024/1024/1024))
-print("torch.cuda.max_memory_reserved: %fGB" %
-      (torch.cuda.max_memory_reserved(0)/1024/1024/1024))
 end_time = time.time()
 print(end_time-start_time)
 print("ready")
 
+with open("C:/Users/User/Desktop/text generation webui/llama + gpt4all Smuggy/convrn.txt", "w") as file:
+	pass
+	file.close()
+with open("C:/Users/User/Desktop/text generation webui/llama + gpt4all Smuggy/convrn.txt", "a") as file:
+	file.write("You are an ai system capable of helping the user with each demand." + "\n")
+	file.write("User:" + "Hello how are you doing?" + "\n")
+	file.write("Smuggy:" + "I'm good. How can i help you?" + "\n")
+	file.close()
 @client.event
 async def on_message(message):
 	print("def loaded")
 	if message.author == client.user:
 		return
+		
+
 #------------------------------------------------
 	if  client.user.mentioned_in(message):
 
@@ -61,7 +62,14 @@ async def on_message(message):
 		global z
 		global new_text_callback
 		new_text_callback = ""
-		context = "User:" + prompt + "response:"
+		with open("C:/Users/User/Desktop/text generation webui/llama + gpt4all Smuggy/convrn.txt", "r") as file:
+			context = file.read()
+			file.close()
+		
+		
+		context2 = (f"\n User: + {prompt} \n + Smuggy:")
+		context = context + context2
+		
 		z = len(context)
 		print("context lenge is in Tokens - Words:", z/4, z)
 
@@ -72,19 +80,25 @@ async def on_message(message):
 		text = ""
 		output = ""
 		start_time = time.time()
-		hey = model.generate(context, n_predict=2)
-		zh = len(prompt)
-		hey = hey[5:]
+		model = Model(ggml_model='gpt4-x-alpaca-13b-native-ggml-model-q4_0.bin')
+		hey = model.generate(context, n_predict=100)
+		zh = len(context)
+		zp = len(prompt)
 		hey = hey[zh:]
-		hey = hey[10:]
+		hey = hey[5:]
+		hey = hey[zp:]
 		await channel.send(hey)
 		end_time = time.time()
 		x = len(us)
 		y = len(text)
 		print("USER:", "Characters:", x, "Tokens:", x/4, "BOT:", "Characters:", y, "Tokens:", y/4, "Time:", end_time - start_time, "|", "MAX:", n+y+x)
-		with open("C://Users/User/Desktop/RWKV/rwkvstic-quant/conversations.txt", "a") as file:
+		with open("C:/Users/User/Desktop/text generation webui/llama + gpt4all Smuggy/conversations.txt", "a") as file:
 			file.write("User:" + prompt + "\n")
-			file.write("Smuggy:" + output + "\n")
+			file.write("Smuggy:" + hey + "\n")
+			file.close()
+		with open("C:/Users/User/Desktop/text generation webui/llama + gpt4all Smuggy/convrn.txt", "a") as file:
+			file.write("User:" + prompt + "\n")
+			file.write("Smuggy:" + hey + "\n")
 			file.close()
 
 client.run(token)
